0f89b7a3f12ccaaa4a8f0e1733ff43ef1157e986
JJhwan7099 <ghks7099@naver.com>
2023-05-26 13:09:14 +0900

    관리자모드에서 메뉴리스트 띄우기까지 했움

--- Software_Project/admin.Designer.cs
+++ Software_Project/admin.Designer.cs
@@ -28,12 +28,31 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(769, 289);
+            this.panel1.TabIndex = 0;
+            // 
+            // admin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(794, 462);
+            this.Controls.Add(this.panel1);
+            this.Name = "admin";
             this.Text = "admin";
+            this.Load += new System.EventHandler(this.admin_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
     }
 }
--- Software_Project/admin.cs
+++ Software_Project/admin.cs
@@ -3,18 +3,67 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Markup;
 
 namespace Software_Project
 {
     public partial class admin : Form
     {
+        List<string[]> mlist = new List<string[]>();
+        ListView menulist = new ListView();
+
+       
         public admin()
         {
             InitializeComponent();
+            mlist.Clear();
+
+
+            menulist.Dock = DockStyle.Fill;
+            menulist.View = View.Details;
+            menulist.Scrollable = true;
+            menulist.FullRowSelect = true;
+            menulist.Columns.Add("Menu");
+            menulist.Columns.Add("가격");
+            menulist.Columns.Add("카테고리");
+
+        }
+
+        private void admin_Load(object sender, EventArgs e)
+        {
+            using(StreamReader reader = new StreamReader("menu.CSV"))
+            {
+                reader.ReadLine();
+                while (!reader.EndOfStream)
+                {
+                    string line;
+                    line = reader.ReadLine();
+                    string[] value = line.Split(',');
+                    mlist.Add(value);
+                }
+                reader.Close();
+            }
+            for( int i = 0; i < mlist.Count; i++ )
+            {
+                ListViewItem item = new ListViewItem(mlist[i][0]);
+                item.SubItems.Add(mlist[i][1]);
+                item.SubItems.Add((string)mlist[i][2]);
+                menulist.Items.Add(item);
+            }
+
+            int colwidth = panel1.ClientSize.Width / menulist.Columns.Count;
+            for (int i = 0; i < menulist.Columns.Count; i++)
+            {
+                ColumnHeader column = menulist.Columns[i];
+                column.Width = colwidth;
+            }
+            panel1.Controls.Add(menulist);
         }
     }
 }
