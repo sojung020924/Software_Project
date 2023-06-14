3b6a95054d1d6ae9e2d1e4226e8c3812da10d257
hyejinll <hyejinlee925@naver.com>
2023-06-15 04:29:46 +0900

    냐옹

--- POS/choolgeun.Designer.cs
+++ POS/choolgeun.Designer.cs
@@ -75,11 +75,11 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("HY견고딕", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.Black;
-            this.label1.Location = new System.Drawing.Point(33, 84);
+            this.label1.Location = new System.Drawing.Point(35, 80);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(209, 34);
+            this.label1.Size = new System.Drawing.Size(207, 46);
             this.label1.TabIndex = 3;
             this.label1.Text = "출근자 이름:";
             this.label1.Click += new System.EventHandler(this.label1_Click);
